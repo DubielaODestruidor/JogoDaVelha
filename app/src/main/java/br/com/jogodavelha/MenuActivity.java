582c6c1dac02f3ff8bab74545d5acc19c9e261fe
João Dubiela <joaopedro.dubiela@hotmail.com>
2022-09-29 20:27:04 -0300

    Projeto final

---
+++ app/src/main/java/br/com/jogodavelha/MenuActivity.java
@@ -0,0 +1,26 @@
+package br.com.jogodavelha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MenuActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        Button btn = (Button)findViewById(R.id.startGameBt);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MenuActivity.this, MainActivity.class));
+            }
+        });
+    }
+}
